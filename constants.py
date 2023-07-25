3a75deb38a106f133fbd4c78034ba7044b48f1c3
Suwaid Aslam <suwaidaslam@gmail.com>
2023-07-25 13:12:34 -0700

    Update constants.py

--- constants.py
+++ constants.py
@@ -6,19 +6,16 @@ cwd = os.getcwd()
 
 IMAGES_FOLDER = os.path.join(cwd, "plot_images")
 PATH_OUT = os.path.join(cwd, "data")
+
+
 # Get the current date and time
-current_date = datetime.now()
+current_date = datetime.utcnow()
 
-current_date = current_date - timedelta(hours=5)
-
-# Subtract 5 days from the current date
-START_DATE = current_date - timedelta(minutes=31)
+START_DATE = current_date.replace(minute=0, second=0, microsecond=0) - timedelta(minutes=60)
 
 # Format the start date in the desired format
 START_DATE = START_DATE.strftime("%Y%m%d-%H%M%S")
 
 
-# Calculate the end date by adding 2 hours and 11 minutes to the current date
-END_DATE = current_date
+END_DATE = current_date.replace(minute=0, second=0, microsecond=0) + timedelta(minutes=61)
 END_DATE = END_DATE.strftime("%Y%m%d-%H%M%S")
-
